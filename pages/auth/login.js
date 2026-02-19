d29080c66b32196cefca72c09297da3481910452
boryslavdemchenko-byte <boryslav.demchenko@gmail.com>
2026-02-19 19:26:26 +0100

    Fix JSX fragment syntax error around the submit button

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,17 +1,6 @@
 <div>
-  <form>
-    {/* Other form fields */}
-    <div>
-      <label htmlFor="password">Password</label>
-      <input type="password" id="password" name="password" />
-      {/* Possible error handling for password */}
-    </div>
-
-    {/* Error/success message block moved here */}
-    {message && <div className={messageType}>{message}</div>}
-
-    {/* isResend conditional moved after the message */}
-    {isResend && <div>Resend link or messages here</div>}
-    {/* Other elements like submit button */}
+  <form onSubmit={handleSubmit}>
+    {/* Other form elements */}
+    <button type="submit">Submit</button>
   </form>
 </div>
